e53893c798c6b2376e57cc6dfab9174fcc85bba6
Shane Logsdon <shane@shanelogsdon.com>
2017-07-17 16:25:30 -0400

    update lightbox params to reflect hpp v2

--- examples/hpp/proxy-request.php
+++ examples/hpp/proxy-request.php
@@ -1,3 +1,11 @@
 <?php
 
-echo file_get_contents('https://dev.rlxcarts.com/mobileSDKs/request.php');
+$json = file_get_contents('https://dev.rlxcarts.com/mobileSDKs/request.php');
+$data = json_decode($json);
+
+$response = array();
+foreach ($data as $key => $value) {
+  $response[$key] = base64_decode($value);
+}
+
+echo json_encode($response);
