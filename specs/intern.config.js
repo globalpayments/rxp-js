3e4eff1a2b0bc53f0918efae4e8adda38f008f81
Shane Logsdon <shane@shanelogsdon.com>
2017-10-04 14:01:35 -0400

    run chrome integration tests headless

--- specs/intern.config.js
+++ specs/intern.config.js
@@ -6,7 +6,12 @@ define({
 	// and platformVersion; any other capabilities options specified for an environment will be copied as-is. Note that
 	// browser and platform names, and version number formats, may differ between cloud testing systems.
 	environments: [
-    { browserName: 'chrome' },
+    {
+			browserName: 'chrome',
+			// Run headless when possible
+			// Comment below line when using `leaveRemoteOpen: true`
+			chromeOptions: { args: ['headless', 'disable-gpu'], }, fixSessionCapabilities: false,
+		},
 	],
 
 	// Uncomment to keep browser automation session open to inspect results of tests
